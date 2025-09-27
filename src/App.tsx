30a4a25033d3bb5f0ca0121feaf0999de6f8ea05
Jenya Shvartsman <jenyas@1touch.io>
2025-09-27 13:49:15 -0400

    feat: integrate Tailwind CSS for styling and update App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <>app</>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg m-4">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">The Star Wars Charts</div>
+          <p className="text-gray-700 text-base">
+            A collection of charts and visualizations about the Star Wars
+            universe.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
